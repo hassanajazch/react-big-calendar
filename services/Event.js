3f80f8447e9df66161ba8dd83d1fc14d3d2a75a2
hassanajaz <hassanajazch@gmail.com>
2019-04-19 14:10:27 +0500

    Add multiple events

--- services/Event.js
+++ services/Event.js
@@ -4,41 +4,37 @@ class Event {
 
     // get events array
     static getEvents() {
-        const now = new Date();
         const events = [
             {
                 id: 0,
                 title: 'Meeting with Markus',
                 allDay: true,
-                start: moment(now).add(1, 'day'),
-                end: moment(now).add(1, 'day'),
+                start: moment().add(1, 'day').hours(1).minutes(0).toDate(),
+                end: moment().add(1, 'day').hours(1).minutes(30).toDate(),
             },
             {
                 id: 1,
                 title: 'Discussion for new projects',
-                start: new Date(2015, 3, 7),
-                end: new Date(2015, 3, 10),
+                start: moment().add(2, 'day').hours(3).minutes(15).toDate(),
+                end: moment().add(2, 'day').hours(3).minutes(45).toDate(),
             },
-
             {
                 id: 2,
                 title: 'Attend conference in Flatiron',
-                start: new Date(2016, 2, 13, 0, 0, 0),
-                end: new Date(2016, 2, 20, 0, 0, 0),
+                start: moment().add(2, 'day').hours(4).minutes(0).toDate(),
+                end: moment().add(2, 'day').hours(6).minutes(0).toDate(),
             },
-
             {
                 id: 3,
                 title: 'Meeting with investors',
-                start: new Date(2016, 10, 6, 0, 0, 0),
-                end: new Date(2016, 10, 13, 0, 0, 0),
+                start: moment().add(3, 'day').hours(2).minutes(15).toDate(),
+                end: moment().add(3, 'day').hours(4).minutes(15).toDate(),
             },
-
             {
                 id: 4,
                 title: 'Birthday Party',
-                start: new Date(2015, 3, 9, 0, 0, 0),
-                end: new Date(2015, 3, 10, 0, 0, 0),
+                start: moment().add(4, 'day').hours(1).minutes(15).toDate(),
+                end: moment().add(4, 'day').hours(3).minutes(30).toDate(),
             },
         ];
 
